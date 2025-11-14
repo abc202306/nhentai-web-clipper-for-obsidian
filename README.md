392232648f02c18b16da2acdd9937be08b9c4481
abc202306 <135524584+abc202306@users.noreply.github.com>
2025-11-14 09:34:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# nhentai-web-clipper-for-obsidian
+
+🔞 A user script that exports NHentai gallery metadata as Obsidian Markdown files (Obsidian NHentai Web Clipper).
+
+A generated markdown note example is shown below
+
+```markdown
+---
+up:
+  - "[[Gallery]]"
+categories:
+  - "[[doujinshi]]"
+keywords:
+  - "[[lolicon]]"
+  - "[[yuri]]"
+  - "[[females-only]]"
+  - "[[rough-translation]]"
+english: "[Ame Usagi (Amedamacon)] Amafuwa Coco Chino Ecchi (Gochuumon wa Usagi Desu ka?) [Chinese] [Digital] (个人机翻汉化)"
+japanese: "[あめうさぎ (飴玉コン)] あまふわココチノえっち (ご注文はうさぎですか?) [中国翻訳] [DL版]"
+url: https://nhentai.net/g/609453
+artist:
+  - "[[amedamacon]]"
+group:
+  - "[[ame-usagi]]"
+parody:
+  - "[[gochuumon-wa-usagi-desu-ka-or-is-the-order-a-rabbit]]"
+character:
+  - "[[chino-kafuu]]"
+  - "[[kokoa-hoto]]"
+language:
+  - "[[translated]]"
+  - "[[chinese]]"
+pagecount: 17
+cover: https://t2.nhentai.net/galleries/3635201/cover.webp.webp
+uploaded: 2025-11-13T10:06:56.332524+00:00
+ctime: 2025-11-13T18:25:41+08:00
+mtime: 2025-11-13T18:25:41+08:00
+---
+
+# 【あめうさぎ (飴玉コン)】 あまふわココチノえっち (ご注文はうさぎですか_) 【中国翻訳】 【DL版】
+
+![200](https://t2.nhentai.net/galleries/3635201/cover.webp.webp)
+
+| | |
+| --- | --- |
+| title_en | `[Ame Usagi (Amedamacon)] Amafuwa Coco Chino Ecchi (Gochuumon wa Usagi Desu ka?) [Chinese] [Digital] (个人机翻汉化)` |
+| title_jp | `[あめうさぎ (飴玉コン)] あまふわココチノえっち (ご注文はうさぎですか?) [中国翻訳] [DL版]` |
+| url | https://nhentai.net/g/609453 |
+| Parodies | [[gochuumon-wa-usagi-desu-ka-or-is-the-order-a-rabbit]] |
+| Characters | [[chino-kafuu]], [[kokoa-hoto]] |
+| Tags | [[lolicon]], [[yuri]], [[females-only]], [[rough-translation]] |
+| Artists | [[amedamacon]] |
+| Groups | [[ame-usagi]] |
+| Languages | [[translated]], [[chinese]] |
+| Categories | [[doujinshi]] |
+| Pages | 17 |
+| Uploaded | 2025-11-13T10:06:56.332524+00:00 |
+```
+
+## How to use
+
+First, install this user script.
+
+Then, when you navigate to the URL https://nhentai.net/g/*, click the "OK" button in the pop-up dialog box to save the current gallery's metadata as an Obsidian Markdown note file.
+
+Additionally, you can download all image files linked to by external image links in the note file using the Obsidian command "Download attachments for current file".
+
+## Screenshots
+
+### 1. The example table view in the Obsidian vault
+
+![alt text](assets/nhentai-webclipper-for-obsidian-image-page1.png) 
+
+### 2. The example use case
+
+![alt text](assets/nhentai-webclipper-for-obsidian-image-page2.png) 
+
+### 3. The example generated markdown note file
+
+![alt text](assets/nhentai-webclipper-for-obsidian-image-page3.png) 
+![alt text](assets/nhentai-webclipper-for-obsidian-image-page4.png) 
+![alt text](assets/nhentai-webclipper-for-obsidian-image-page5.png)
---
+++ src/nhentai-webclipper-for-obsidian.js
@@ -0,0 +1,180 @@
+// ==UserScript==
+// @name         NHentai Web Clipper for Obsidian
+// @namespace    https://nhentai.net
+// @version      v1.0.4.20251114
+// @description  🔞 A user script that exports NHentai gallery metadata as Obsidian Markdown files (Obsidian NHentai Web Clipper).
+// @author       abc202306
+// @match        https://nhentai.net/g/*
+// @icon         none
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Entry point
+  setTimeout(startNHentaiWebclipper, 2000);
+
+  function startNHentaiWebclipper() {
+    const data = getData();
+    const fileContent = getFileContent(data);
+    const obsidianURI = getObsidianURI(data.title, fileContent);
+
+    if (confirm("Do you want to proceed?")) {
+      window.location.href = obsidianURI;
+    }
+  }
+
+  // Build Obsidian URI
+  function getObsidianURI(title, fileContent) {
+    const params = [
+      ["file", `acg/galleries/${title}`],
+      ["content", fileContent],
+      ["append", "1"]
+    ].map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join("&");
+
+    return `obsidian://new?${params};`;
+  }
+
+  // Extract metadata from page
+  function getData() {
+    const info = document.querySelector("#info");
+    const titles = info.querySelectorAll(".title");
+
+    const now = getLocalISOStringWithTimezone();
+
+    const data = {
+      title: sanitizeTitle(getTitleStr(titles[1])),
+      english: getTitleStr(titles[0]),
+      japanese: getTitleStr(titles[1]),
+      url: `https://nhentai.net/g/${info.querySelector("#gallery_id").innerText.slice(1)}`,
+      cover: document.querySelector("#cover img").src,
+      parody: [],
+      character: [],
+      keywords: [],
+      artist: [],
+      group: [],
+      language: [],
+      categories: [],
+      pagecount: null,
+      uploaded: null,
+      ctime: now,
+      mtime: now
+    };
+
+    const keyMap = {
+      Parodies: "parody",
+      Characters: "character",
+      Tags: "keywords",
+      Artists: "artist",
+      Groups: "group",
+      Languages: "language",
+      Categories: "categories"
+    };
+
+    info.querySelectorAll("#tags > div.tag-container").forEach(tagGroupCon => {
+      const groupName = getTagGroupName(tagGroupCon);
+      if (groupName === "Uploaded") {
+        data.uploaded = tagGroupCon.querySelector("time").dateTime;
+      } else if (groupName === "Pages") {
+        data.pagecount = getTagName(tagGroupCon.querySelector(".name"));
+      } else if (keyMap[groupName]) {
+        data[keyMap[groupName]] = [...tagGroupCon.querySelectorAll(".name")]
+          .map(el => `[[${getTagName(el)}]]`);
+      }
+    });
+
+    return data;
+  }
+
+  // Build Obsidian note content
+  function getFileContent(data) {
+    const escapePipe = str => str.replace(/\|/g, "\\|");
+
+    return `---
+up:
+  - "[[Gallery]]"
+categories:${getYamlArrayStr(data.categories)}
+keywords:${getYamlArrayStr(data.keywords)}
+english: "${data.english}"
+japanese: "${data.japanese}"
+url: "${data.url}"
+artist:${getYamlArrayStr(data.artist)}
+group:${getYamlArrayStr(data.group)}
+parody:${getYamlArrayStr(data.parody)}
+character:${getYamlArrayStr(data.character)}
+language:${getYamlArrayStr(data.language)}
+pagecount: ${data.pagecount}
+cover: "${data.cover}"
+uploaded: ${data.uploaded}
+ctime: ${data.ctime}
+mtime: ${data.mtime}
+---
+
+# ${data.title}
+
+![200](${data.cover})
+
+| | |
+| --- | --- |
+| title_en | \`${escapePipe(data.english)}\` |
+| title_jp | \`${escapePipe(data.japanese)}\` |
+| url | ${data.url} |
+| Parodies | ${data.parody.join(", ")} |
+| Characters | ${data.character.join(", ")} |
+| Tags | ${data.keywords.join(", ")} |
+| Artists | ${data.artist.join(", ")} |
+| Groups | ${data.group.join(", ")} |
+| Languages | ${data.language.join(", ")} |
+| Categories | ${data.categories.join(", ")} |
+| Pages | ${data.pagecount} |
+| Uploaded | ${data.uploaded} |
+`;
+  }
+
+  // Helpers
+  function sanitizeTitle(str) {
+    return str
+      .replaceAll("[", "【")
+      .replaceAll("]", "】")
+      .replaceAll(/[?:]/g, "_");
+  }
+
+  function getTitleStr(titleEl) {
+    if (!titleEl) return "";
+    return [".before", ".pretty", ".after"]
+      .map(sel => titleEl.querySelector(sel)?.innerText.trim() || "")
+      .join(" ")
+      .replace(/\s{2,}/g, " ");
+  }
+
+  function getTagGroupName(tagGroupCon) {
+    return tagGroupCon.firstChild.textContent.trim().replace(/:$/, "");
+  }
+
+  function getTagName(tagNameEl) {
+    return tagNameEl.innerText.trim()
+      .replace(/\s+/g, "-")
+      .replace("-|-", "-or-");
+  }
+
+  function getLocalISOStringWithTimezone() {
+    const date = new Date();
+    const pad = n => String(n).padStart(2, "0");
+
+    const offset = -date.getTimezoneOffset(); // actual UTC offset in minutes
+    const sign = offset >= 0 ? "+" : "-";
+    const hours = pad(Math.floor(Math.abs(offset) / 60));
+    const minutes = pad(Math.abs(offset) % 60);
+
+    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T` +
+      `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}` +
+      `${sign}${hours}:${minutes}`;
+  }
+
+  function getYamlArrayStr(arr) {
+    return arr.map(i => `\n  - "${i}"`).join("");
+  }
+
+})();
